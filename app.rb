652779338b4cd03d235e0f15f1b9e982b9369d3e
Vincent Langat <kimutailangat033@gmail.com>
2022-08-19 16:27:26 +0300

    commit tree acf34285ec15ce96e2914d4bb71127171a557411

---
+++ app.rb
@@ -0,0 +1,16 @@
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
+puts "Hello world!"
+puts "Hello world!"
+print "Hello  world"
+print "Hello  world"
+print "Hello  world"
+print "Hello  world"
+
+p "Helloworld  "
+p "Helloworld  "
+p "Helloworld  "
+p "Helloworld  "
+pp "Helloworld!  "
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
